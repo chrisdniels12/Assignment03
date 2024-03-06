da00c5c53f212b4de860fa751a81fbc44eda91ed
Chris <chrisdniels12@gmail.com>
2024-03-06 13:41:45 -0800

    Update 03-triangle.js
    finish the looping triangle assignment

---
+++ js/03-triangle.js
@@ -0,0 +1,12 @@
+//  triangle limitter, start point and the output
+let triangleNum = 6;
+let i = 0;
+let output = ""
+
+do {
+    //  accumulate the output and log it to the console while the condition is true
+    output += "#"
+    console.log(output)
+    i++
+    //  continue to do this while i is less than 6
+} while (i < triangleNum)
